1792d92c124d1552949ae292c5d5a5b26bfa5374
coder-goying <13040837899@163.com>
2019-02-17 10:39:48 +0800

    Initial commit

---
+++ src/test/java/com/renchaigao/zujuba/photoserver/PhotoserverApplicationTests.java
@@ -0,0 +1,17 @@
+package com.renchaigao.zujuba.photoserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
